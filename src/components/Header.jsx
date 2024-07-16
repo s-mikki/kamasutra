e8a20c2d5665c8c67122075b450c2102aa67a13b
Mirlan Almazovich <strong.mirlan99@gmail.com>
2024-07-16 21:00:09 +0600

    1

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="header__img" width="500"
+                 src="https://logos-world.net/wp-content/uploads/2020/05/Huawei-Logo.jpg" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
